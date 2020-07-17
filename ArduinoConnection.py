1fa966de1180962b94b6804ae76f3f21a57ba89c
Edwin Mwiti Maingi <45706040+edwinmwiti@users.noreply.github.com>
2020-07-17 17:01:37 +0300

    Update ArduinoConnection.py

--- ArduinoConnection.py
+++ ArduinoConnection.py
@@ -1,1 +1,16 @@
+from pyfirmata import Arduino, util
+from time import sleep
+import serial
+
+
+def create_arduino_connection(port='COM13'):
+    try:
+        port = port
+        board = Arduino(port)
+        sleep(3)
+        it = util.Iterator()
+        it.start()
+        return True
+    except serial.serialutil.SerialException:
+        return False
 
